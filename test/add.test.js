08752e2cc37486a492fa8d9939d8f95089ce0bd7
Sarah Morris <sarah.morris9@gmail.com>
2018-03-10 17:27:02 -0600

    add feature working

--- test/add.test.js
+++ test/add.test.js
@@ -2,7 +2,10 @@ const add = require('../js/add');
 const {assert:{isFunction, equal}} = require('chai');
 
 describe('adding', () => {
-    it('should be a function', => {
+    it('should be a function', () => {
         isFunction(add);
+    })
+    it('should add the arguments', () => {
+        equal(10, add(5, 5));
     })
 })
